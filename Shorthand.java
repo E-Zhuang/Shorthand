30e05c5c200b5c02e3332394d6d73886d3b0a26d
E-Zhuang <springreading@gmail.com>
2016-10-16 23:14:13 -0700

    Add files via upload

--- Shorthand.java
+++ Shorthand.java
@@ -96,178 +96,35 @@ public class Shorthand
         tag = getNextTag();
         
         while (tag != "")
-        {
-            if (!tag.equalsIgnoreCase("for") && !tag.equalsIgnoreCase("and") && 
-                !tag.equalsIgnoreCase("to") && !tag.equalsIgnoreCase("you"))
-            {
-                removeAll("aeiouAEIOU");
-                output += tag;
-            }
+        {   
+            
+            if (tag.equalsIgnoreCase("for"))
+                    output += '4';
             else
             {
-                if (tag.equalsIgnoreCase("for"))
-                    output += '4';
                 if (tag.equalsIgnoreCase("and"))
                     output += '&';
-                if (tag.equalsIgnoreCase("to"))
-                    output += '2';
-                if (tag.equalsIgnoreCase("you"))
-                    output += 'U';
-            }    
+                else
+                {
+                    if (tag.equalsIgnoreCase("to"))
+                        output += '2';
+                    else 
+                    {
+                        if (tag.equalsIgnoreCase("you"))
+                            output += 'U';
+                        else
+                        {
+                            removeAll("aeiouAEIOU");
+                            output += tag;
+                        }
+                    }
+                }
+            }
             
             tag = getNextTag();
         }
         
         return output;
-    }
-    
-    public String newToShortHand()
-    {
-        String shorthand = "";
-        char character1, character2, character3;
-        
-        for (int letter = 0; letter < input.length(); letter++)
-        {
-            character1 = input.charAt(letter);
-            if (character1 == 'f' || character1 == 'F')
-            {
-                character2 = input.charAt(letter + 1);
-                if (character2 == 'o' || character2 == 'O')
-                {
-                    character3 = input.charAt(letter + 2);
-                    if (character3 == 'r' || character3 == 'R')
-                    {
-                        shorthand += '4';
-                    }
-                    else
-                    {
-                        shorthand += character1 + character2 + character3;
-                    }
-                }
-                else
-                    shorthand += character1 +character2;
-            }
-            else
-                shorthand += character1;
-                
-            if (character1 == 'a' || character1 == 'A')
-            {
-                letter++;
-                character2 = input.charAt(letter);
-                if (character2 == 'n' || character2 == 'N')
-                {
-                    letter++;
-                    character3 = input.charAt(letter);
-                    if (character3 == 'd' || character3 == 'D')
-                    {
-                        shorthand += '&';
-                        letter++;
-                    }
-                    else
-                    {
-                        shorthand += character1 + character2 + character3;
-                        letter++;
-                    }
-                }
-                else
-                    shorthand += character1 +character2;
-            }
-            else
-                shorthand += character1;
-        
-        
-            if (character1 == 'y' || character1 == 'Y')
-                {
-                    letter++;
-                    character2 = input.charAt(letter);
-                    if (character2 == 'o' || character2 == 'O')
-                    {
-                        letter++;
-                        character3 = input.charAt(letter);
-                        if (character3 == 'u' || character3 == 'U')
-                        {
-                            shorthand += 'U';
-                            letter++;
-                        }
-                        else
-                        {
-                            shorthand += character1 + character2 + character3;
-                            letter++;
-                        }
-                    }
-                    else
-                        shorthand += character1 +character2;
-                }
-                else
-                    shorthand += character1;
-                    
-            if (character1 == 't' || character1 == 'T')
-                {
-                    letter++;
-                    character2 = input.charAt(letter);
-                    if (character2 == 'o' || character2 == 'O')
-                    {
-                        letter++; 
-                        shorthand += '2';
-                    }
-                    else
-                    {
-                        shorthand += character1 +character2;
-                        letter++;
-                    }
-                }
-                else
-                    shorthand += character1;
-                    
-        }
-        
-        return shorthand;
-    }
-    
-        public String toShorthand()
-    {
-        String first, last;
-        int search;
-        
-        search = newIn.indexOf(" and ");
-        while (search != -1)
-        {
-            first = newIn.substring(0, search + 1);
-            last = newIn.substring(search + 4, newIn.length());
-            newIn = first + "&" + last;
-            search = newIn.indexOf(" and ");
-        }
-        
-        search = newIn.indexOf(" for ");
-        while (search != -1)
-        {
-            first = newIn.substring(0, search + 1);
-            last = newIn.substring(search + 4, newIn.length());
-            newIn = first + "4" + last;
-            search = newIn.indexOf(" for ");
-        }
-        
-        search = newIn.indexOf(" you ");
-        while (search != -1)
-        {
-            first = newIn.substring(0, search + 1);
-            last = newIn.substring(search + 4, newIn.length());
-            newIn = first + "U" + last;
-            search = newIn.indexOf(" you ");
-        }
-        
-        search = newIn.indexOf(" to ");
-        while (search != -1)
-        {
-            first = newIn.substring(0, search + 1);
-            last = newIn.substring(search + 3, newIn.length());
-            newIn = first + "2" + last;
-            search = newIn.indexOf(" to ");
-        }
-        
-        removeAll("aeiou");
-        
-        return newIn;
     }
 
 }
