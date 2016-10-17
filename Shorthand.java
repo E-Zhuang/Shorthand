a8a822e495e1e3462a451a3daf00c4cf468047d6
E-Zhuang <springreading@gmail.com>
2016-10-16 23:05:56 -0700

    Add files via upload

--- Shorthand.java
+++ Shorthand.java
@@ -8,17 +8,223 @@
 
 public class Shorthand
 {
-    private String input, newIn;
-    private int length;
+    private String input, newIn, tag;
+    private int length, nextTagIndex;
     
-    public Shorthand(String input)
+    public Shorthand(String in)
     {
-        input = input;
-        newIn = " " + input + " ";
-        length = newIn.length();
+        input = in;
+        length = input.length();
+        nextTagIndex = 0;
+        tag = "";
     }
     
-    public String toShorthand()
+    public void removeAll(String remove)
+    {
+        String in, test;
+        int characters, length;
+            
+        in = remove;
+        
+        for (int many = 0; many < in.length(); many++)
+        {
+            // instantiates an empty string and fills it with a character
+            test = "";
+            test += in.charAt(many);
+                
+            // tests to see if there is a punctuation mark of type "test"
+            characters = tag.indexOf(test);
+            // removes the punctuation mark as long as there is one
+            while (characters != -1)
+            {
+                length = tag.length(); 
+                tag = tag.substring (0, characters) + tag.substring ((characters + 1), (length));
+                characters = tag.indexOf(test);
+            }
+        }
+        
+    }
+        
+    public String getNextTag()
+    {
+        String nextTag = "";
+        char letter;
+        int offset;
+        
+        offset = 0;
+        
+        if (nextTagIndex < input.length())
+        {
+            letter = input.charAt(nextTagIndex + offset);
+            if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+            {
+                while ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+                {
+                    nextTag += letter;
+                    offset++;
+                    
+                    //checks if the character is in the string  
+                    if((nextTagIndex + offset) < input.length())
+                    {
+                        letter = input.charAt(nextTagIndex + offset);
+                    }
+                    //if the character is not part of the string (at the end), then letter = 0
+                    //which breaks the loop
+                    else
+                        letter = 0;
+                        
+                }
+
+                nextTagIndex += offset;
+            }
+            else
+            {
+                nextTag += letter;
+                nextTagIndex++;
+            }
+        }
+        
+        return nextTag;
+    }
+    
+    public String newShorthand()
+    {
+        String output;
+        
+        output = "";
+        
+        tag = getNextTag();
+        
+        while (tag != "")
+        {
+            if (!tag.equalsIgnoreCase("for") && !tag.equalsIgnoreCase("and") && 
+                !tag.equalsIgnoreCase("to") && !tag.equalsIgnoreCase("you"))
+            {
+                removeAll("aeiouAEIOU");
+                output += tag;
+            }
+            else
+            {
+                if (tag.equalsIgnoreCase("for"))
+                    output += '4';
+                if (tag.equalsIgnoreCase("and"))
+                    output += '&';
+                if (tag.equalsIgnoreCase("to"))
+                    output += '2';
+                if (tag.equalsIgnoreCase("you"))
+                    output += 'U';
+            }    
+            
+            tag = getNextTag();
+        }
+        
+        return output;
+    }
+    
+    public String newToShortHand()
+    {
+        String shorthand = "";
+        char character1, character2, character3;
+        
+        for (int letter = 0; letter < input.length(); letter++)
+        {
+            character1 = input.charAt(letter);
+            if (character1 == 'f' || character1 == 'F')
+            {
+                character2 = input.charAt(letter + 1);
+                if (character2 == 'o' || character2 == 'O')
+                {
+                    character3 = input.charAt(letter + 2);
+                    if (character3 == 'r' || character3 == 'R')
+                    {
+                        shorthand += '4';
+                    }
+                    else
+                    {
+                        shorthand += character1 + character2 + character3;
+                    }
+                }
+                else
+                    shorthand += character1 +character2;
+            }
+            else
+                shorthand += character1;
+                
+            if (character1 == 'a' || character1 == 'A')
+            {
+                letter++;
+                character2 = input.charAt(letter);
+                if (character2 == 'n' || character2 == 'N')
+                {
+                    letter++;
+                    character3 = input.charAt(letter);
+                    if (character3 == 'd' || character3 == 'D')
+                    {
+                        shorthand += '&';
+                        letter++;
+                    }
+                    else
+                    {
+                        shorthand += character1 + character2 + character3;
+                        letter++;
+                    }
+                }
+                else
+                    shorthand += character1 +character2;
+            }
+            else
+                shorthand += character1;
+        
+        
+            if (character1 == 'y' || character1 == 'Y')
+                {
+                    letter++;
+                    character2 = input.charAt(letter);
+                    if (character2 == 'o' || character2 == 'O')
+                    {
+                        letter++;
+                        character3 = input.charAt(letter);
+                        if (character3 == 'u' || character3 == 'U')
+                        {
+                            shorthand += 'U';
+                            letter++;
+                        }
+                        else
+                        {
+                            shorthand += character1 + character2 + character3;
+                            letter++;
+                        }
+                    }
+                    else
+                        shorthand += character1 +character2;
+                }
+                else
+                    shorthand += character1;
+                    
+            if (character1 == 't' || character1 == 'T')
+                {
+                    letter++;
+                    character2 = input.charAt(letter);
+                    if (character2 == 'o' || character2 == 'O')
+                    {
+                        letter++; 
+                        shorthand += '2';
+                    }
+                    else
+                    {
+                        shorthand += character1 +character2;
+                        letter++;
+                    }
+                }
+                else
+                    shorthand += character1;
+                    
+        }
+        
+        return shorthand;
+    }
+    
+        public String toShorthand()
     {
         String first, last;
         int search;
@@ -63,30 +269,5 @@ public class Shorthand
         
         return newIn;
     }
-        
-    
-    
-    public void removeAll(String input)
-    {
-        String in = input, test;
-        int characters;
-            
-        for (int many = 0; many < in.length(); many++)
-        {
-            // instantiates an empty string and fills it with a punctuation mark
-            test = "";
-            test += in.charAt(many);
-                
-            // tests to see if there is a punctuation mark of type "many"
-            characters = newIn.indexOf(test);
-            // removes the punctuation mark as long as there is one
-            while (characters != -1)
-            {
-                length = newIn.length(); 
-                newIn = newIn.substring (0, characters) + newIn.substring ((characters + 1), (length));
-                characters = newIn.indexOf(test);
-            }
-        }
-        
-    }
+
 }
--- ShorthandTester.java
+++ ShorthandTester.java
@@ -16,21 +16,32 @@ public class ShorthandTester
         ConsoleIO keyboard = new ConsoleIO();
         Shorthand shrthnd;
         
-        String input, deltaInput, output;
+        String input, output, tag;
         
         //Prompts the user for a string to convert into shorthand
-        System.out.print("Enter a string to be converted into shorthand ");
-        System.out.print("(Enter Q or q to quit): ");
-        input = keyboard.readLine();
-        
-        //converts the string to all lowercase and instantiates Shorthand class
-        deltaInput = input.toLowerCase();
-        shrthnd = new Shorthand(deltaInput);
-        output = shrthnd.toShorthand();
-        System.out.println(output);
-        
-        
-        
+       System.out.print("Enter a string to be converted into shorthand ");
+       System.out.print("(Enter Q or q to quit): ");
+       //input = keyboard.readLine();
+       
+       //test code
+       shrthnd = new Shorthand("You can pretend to be serious; you can't pretend to be witty");
+       if (shrthnd.newShorthand().equals("U cn prtnd 2 b srs; U cn't prtnd 2 b wtty")) 
+            System.out.println("true");
+       else 
+            System.out.println("false");
+       
+        //instantiates Shorthand class
+//         while (!input.equalsIgnoreCase("q"))
+//         {
+//             shrthnd = new Shorthand(input);
+//             output = shrthnd.newShorthand();
+//             System.out.println(output);
+//             System.out.println();
+//             
+//             System.out.print("Enter a string to be converted into shorthand ");
+//             System.out.print("(Enter Q or q to quit): ");
+//             input = keyboard.readLine();
+//         }
 
     }
 }
